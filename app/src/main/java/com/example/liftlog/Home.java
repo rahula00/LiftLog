fa481934b489fb699c2f128eb11db8bb46cd9469
rahul <rahul-arora@outlook.com>
2021-01-16 17:35:17 -0800

    removed 2x back on home page

--- app/src/main/java/com/example/liftlog/Home.java
+++ app/src/main/java/com/example/liftlog/Home.java
@@ -33,20 +33,9 @@ public class Home extends AppCompatActivity {
      * Back button listener.
      * Will close the application if the back button pressed twice.
      */
-    boolean doubleBackToExitPressedOnce = false;
     @Override
     public void onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed();
-            return;
-        }
-        this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Click BACK again to exit", Toast.LENGTH_SHORT).show();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                doubleBackToExitPressedOnce=false;
-            }
-        }, 4000);
+        super.onBackPressed();
+        return;
     }
 }
