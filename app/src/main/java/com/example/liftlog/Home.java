443a224557b3ad020100a041e322dedd62b96535
rahul <rahul-arora@outlook.com>
2021-01-16 17:20:57 -0800

    removed 2x back press to exit

--- app/src/main/java/com/example/liftlog/Home.java
+++ app/src/main/java/com/example/liftlog/Home.java
@@ -3,7 +3,8 @@ package com.example.liftlog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.*;
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
