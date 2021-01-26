6aa881d45b7a5e1044ef2755e895b5e35cbeef94
Ishan <ishanpathirana5@gmail.com>
2021-01-25 21:55:47 -0800

    Updated ExerciseStats to reflect model

--- app/src/main/java/com/example/liftlog/ExerciseStats.java
+++ app/src/main/java/com/example/liftlog/ExerciseStats.java
@@ -1,15 +1,15 @@
 package com.example.liftlog;
 
 public class ExerciseStats {
-    public int max_weight;
+    public int exercise;
+    public int weight;
     public int reps;
     public int sets;
-    public int exercise_id;
 
-    public ExerciseStats(int maxweight, int numreps, int numsets, int id){
-        max_weight = maxweight;
+    public ExerciseStats(int id, int weightassigned, int numreps, int numsets){
+        exercise = id;
+        weight = weightassigned;
         reps = numreps;
         sets = numsets;
-        exercise_id = id;
     }
 }
