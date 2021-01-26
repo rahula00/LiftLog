09a2ae11ad561fd6d4d2b0dbadd8f41256e491ab
Ishan <ishanpathirana5@gmail.com>
2021-01-25 22:12:23 -0800

    Adjusted Exercise class to reflect new model

--- app/src/main/java/com/example/liftlog/Exercise.java
+++ app/src/main/java/com/example/liftlog/Exercise.java
@@ -8,16 +8,18 @@ import android.widget.ImageView;
 import java.io.IOException;
 
 public class Exercise {
-    // exercise id & instructions
+    // exercise id, name, & instructions
     public int ID;
+    public String name;
     public String instructions;
     // for taking the specific image for the exercise and setting it as an ImageView
-    public Context context;
+    public static Context context;
     public Bitmap image;
     public ImageView description;
 
-    public Exercise(int id, String directions, String assetName) {
+    public Exercise(int id, String exerciseName, String directions, String assetName) {
         ID = id;
+        name = exerciseName;
         instructions = directions;
         // using open() requires this try/catch statement but probably won't throw an error
         try {
