0393557b8cfa7a110725d413db396e9ea37f43b1
YotamGolan <golanyotam@gmail.com>
2021-02-03 09:47:23 -0800

    Update Routine.java

--- app/src/main/java/com/example/liftlog/Routine.java
+++ app/src/main/java/com/example/liftlog/Routine.java
@@ -7,13 +7,15 @@ public class Routine {
     public int id;
     public String name;
     public String description;
+    public String imageUrl;
     public Queue<Integer> workouts;           //The ids of the workouts in the routine
 
-    public Routine(int id, String routineName, String description, Queue<Integer> workouts)
+    public Routine(int id, String routineName, String description, String imageUrl, Queue<Integer> workouts)
     {
         this.id = id;
         this.name = routineName;
         this.description = description;
+        this.imageUrl = imageUrl;
         this.workouts = workouts;
     }
 }
