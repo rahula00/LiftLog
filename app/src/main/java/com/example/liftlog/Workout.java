a71fdd28bb30ca35acdef776b74ffb70d43e85a8
YotamGolan <golanyotam@gmail.com>
2021-02-02 19:26:33 -0800

    Update Workout.java

--- app/src/main/java/com/example/liftlog/Workout.java
+++ app/src/main/java/com/example/liftlog/Workout.java
@@ -7,14 +7,14 @@ public class Workout {
     public String name;
     public String description;
     public List<Integer> triggers;                          //The ids of the exercises to increase max weight
-    public List<ExerciseStats> exercises_stats;             //The exercise list
+    public List<ExerciseStats> statsList;               //The exercise list
 
-    public Workout(int id, String workoutName, String description, List<Integer> triggers, List<ExerciseStats> exercises_stats)
+    public Workout(int id, String workoutName, String description, List<Integer> triggers, List<ExerciseStats> exercisesStats)
     {
         this.id = id;
         this.name = workoutName;
         this.description = description;
         this.triggers = triggers;
-        this.exercises_stats = exercises_stats;
+        this.statsList = exercisesStats;
     }
 }
