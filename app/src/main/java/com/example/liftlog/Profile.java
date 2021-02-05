c5706dd93c81c91668d921cc3df61a96be233b75
rahul <rahul-arora@outlook.com>
2021-02-04 17:14:50 -0800

    Manifest permissions.
    - added text box color
    - split profile texts into 3 pieces. added "____" textbox for max weight
    - working on selecting profile pic from camera

--- app/src/main/java/com/example/liftlog/Profile.java
+++ app/src/main/java/com/example/liftlog/Profile.java
@@ -1,26 +1,68 @@
 package com.example.liftlog;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class Profile extends AppCompatActivity {
+    EditText mName,mDob, mEmail;
+    Button mSaveBtn;
+    TextView mMaxWeight;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+
+    ImageView iv;
+    Integer REQUEST_CAMERA = 1, SELECT_FILE=0;
+
+
     EditText yourEditText;
     int mYear, mMonth, mDay;
+
+
+//    private void SelectImage() {
+//        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(Profile.this);
+//        builder.setTitle("Add Image");
+//        builder.setItems(items, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int i) {
+//                if (items[i].equals("Camera")) {
+//                }
+//                else if(items[i].equals("Gallery")) {
+//                }
+//                else if(items[i].equals("Cancel")) {
+//                }
+//            }
+//        })
+//    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        iv = (ImageView) findViewById(R.id.profile_image);
+
 
         yourEditText = (EditText) findViewById(R.id.dob);
         yourEditText.setOnClickListener(new View.OnClickListener() {
