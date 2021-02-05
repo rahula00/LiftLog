65242f2bbf2f8069fce6d1477d1e3574da220ee7
Ishan <ishanpathirana5@gmail.com>
2021-02-04 18:47:48 -0800

    Added functional Navigation page with new pages

--- app/src/main/java/com/example/liftlog/MainActivity.java
+++ app/src/main/java/com/example/liftlog/MainActivity.java
@@ -16,15 +16,67 @@ import com.google.firebase.auth.*;
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
     Button mLogoutBtn;
+    Button mProfileBtn;
+    Button mRoutinesBtn;
+    Button mProgressBtn;
+    Button mMyWorkoutsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProfileBtn = findViewById(R.id.btnProfile);
+        mRoutinesBtn = findViewById(R.id.btnRoutines);
+        mProgressBtn = findViewById(R.id.btnProgress);
+        mMyWorkoutsBtn = findViewById(R.id.btnMyWorkouts);
         mLogoutBtn = findViewById(R.id.btnLogout);
         fAuth = FirebaseAuth.getInstance();
 
-//On click of 'Log Out' button
+        //On click of 'Profile' button
+        mProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Profile.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //On click of 'Progress' button
+        mRoutinesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Routines.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //On click of 'Progress' button
+        mProgressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Progress.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //On click of 'Progress' button
+        mMyWorkoutsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyWorkouts.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //On click of 'Log Out' button
         mLogoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
