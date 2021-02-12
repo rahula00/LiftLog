60f7d1720726d2d68b1be50006219c1d4368a432
Ishan <ishanpathirana5@gmail.com>
2021-02-12 08:21:35 -0800

    Got list to work

--- app/src/main/java/com/example/liftlog/MyWorkouts.java
+++ app/src/main/java/com/example/liftlog/MyWorkouts.java
@@ -6,12 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyWorkouts extends AppCompatActivity {
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_workouts);
+
+        listView = findViewById(R.id.listView);
+        ArrayList<Exercise> arrayList = new ArrayList<>();
+        arrayList.add(new Exercise(1,"BenchPress", "Hold the bar"));
+        arrayList.add(new Exercise(2,"Curls", "Curl the thing" ));
+        arrayList.add(new Exercise(3,"LegPress", "Push with legs"));
+        arrayList.add(new Exercise(4,"BenchPress", "Hold the bar"));
+        arrayList.add(new Exercise(5,"Curls", "Curl the thing" ));
+        arrayList.add(new Exercise(6,"LegPress", "Push with legs"));
+        arrayList.add(new Exercise(7,"Incline", "Hold the bar"));
+        WorkoutExerciseAdapter adapter = new WorkoutExerciseAdapter(this, R.layout.myworkoutlistelement, arrayList);
+        listView.setAdapter(adapter);
     }
 
     //Handle back buttonfunctionalities.
