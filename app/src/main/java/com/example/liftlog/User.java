d7b009e2663df012246cf1bf7f188dda17393730
Scott Zin <scottzin7@gmail.com>
2021-02-12 17:15:37 -0800

    removed database functions

--- app/src/main/java/com/example/liftlog/User.java
+++ app/src/main/java/com/example/liftlog/User.java
@@ -21,10 +21,10 @@ import java.util.Queue;
 import java.util.regex.Pattern;
 
 public class User extends AppCompatActivity {
-    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference database = firebaseDatabase.getReference().child("Users");
-    FirebaseAuth fAuth = FirebaseAuth.getInstance();
-    String UID = fAuth.getCurrentUser().getUid();
+//    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//    DatabaseReference database = firebaseDatabase.getReference().child("Users");
+//    FirebaseAuth fAuth = FirebaseAuth.getInstance();
+//    String UID = fAuth.getCurrentUser().getUid();
     public String email;
     public Bitmap profile_pic;
     public String name;
@@ -60,7 +60,7 @@ public class User extends AppCompatActivity {
         Pattern p = Pattern.compile(emailRegex);
         if(p.matcher(nEmail).matches()){
             email = nEmail;
-            database.child(UID).child("Email").setValue(email);
+//            database.child(UID).child("Email").setValue(email);
             return true;
         }
         return false;
@@ -68,7 +68,7 @@ public class User extends AppCompatActivity {
 
     void setName(String nName){
         name = nName;
-        database.child(UID).child("Name").setValue(name);
+//        database.child(UID).child("Name").setValue(name);
     }
 
     boolean setDate(Integer year, Integer month, Integer day){
@@ -76,7 +76,7 @@ public class User extends AppCompatActivity {
         birthDate.set(year, month, day);
         //kind of a bad way to check the date. checks if the day they put in was less than the current date
         if(birthDate.get(Calendar.DAY_OF_YEAR) < cal.get(Calendar.DAY_OF_YEAR) && birthDate.get(Calendar.YEAR) < cal.get(Calendar.YEAR)){
-            database.child(UID).child("Date").setValue(birthDate);
+//            database.child(UID).child("Date").setValue(birthDate);
             return true;
         }
         return false;
@@ -84,14 +84,14 @@ public class User extends AppCompatActivity {
 
     void setSex(boolean nSex){
         sex = nSex;
-        database.child(UID).child("Sex").setValue(sex);
+//        database.child(UID).child("Sex").setValue(sex);
     }
 
     boolean setHeight(Integer feet, Integer inches){
         //high doubts you are going to be less than 3 feet and working out
         if(feet > 3){
             height = new Pair<Integer,Integer>(feet,inches);
-            database.child(UID).child("Height").setValue(height);
+//            database.child(UID).child("Height").setValue(height);
             return true;
         }
         return false;
@@ -100,7 +100,7 @@ public class User extends AppCompatActivity {
     boolean setWeight(float nWeight) {
         if(nWeight>0) {
             weight = nWeight;
-            database.child(UID).child("Weight").setValue(weight);
+//            database.child(UID).child("Weight").setValue(weight);
             return true;
         }
         return false;
@@ -108,7 +108,7 @@ public class User extends AppCompatActivity {
 
     void setRoutine_id(Integer id){
         routine_id = id;
-        database.child(UID).child("Routine_id").setValue(id);
+//        database.child(UID).child("Routine_id").setValue(id);
     }
 
     boolean setUser_max(Integer id, Integer weight){
@@ -117,7 +117,7 @@ public class User extends AppCompatActivity {
             ArrayList<Pair<Calendar,Integer>> current_max_list = this.user_max.get(id);
             current_max_list.add(new_max);
             this.user_max.put(id,current_max_list);
-            database.child(UID).child("User_max").setValue(user_max);
+//            database.child(UID).child("User_max").setValue(user_max);
             return true;
         }
         return false;
