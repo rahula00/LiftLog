adf922fdaa496ae3cd856f2c401c339c3222b427
rahul <rahul-arora@outlook.com>
2021-02-17 17:58:07 -0800

    Add user to myapplication

--- app/src/main/java/com/example/liftlog/MyApplication.java
+++ app/src/main/java/com/example/liftlog/MyApplication.java
@@ -5,11 +5,13 @@ import android.content.Context;
 
 public class MyApplication extends Application {
     private static Context context;
+    public static User user;
 
     public void onCreate() {
         super.onCreate();
         MyApplication.context = getApplicationContext();
     }
+
 
     public static Context getAppContext(){
         return MyApplication.context;
