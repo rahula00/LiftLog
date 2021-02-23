35bc32e417fb2fa5cb1016dced5eef4114cffbc5
Scott Zin <scottzin7@gmail.com>
2021-02-22 23:22:59 -0800

    brought this branch up to date

--- app/src/main/java/com/example/liftlog/RoutineView.java
+++ app/src/main/java/com/example/liftlog/RoutineView.java
@@ -5,34 +5,28 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Calendar;
-
 public class RoutineView extends AppCompatActivity {
-    FirebaseAuth fAuth;
     Button mStartBtn;
-    User myUser;
-    Routine routine;
     TextView mName, mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine);
-        fAuth = FirebaseAuth.getInstance();
+        MyApplication.fAuth = FirebaseAuth.getInstance();
         mStartBtn = findViewById(R.id.startButton);
 
         mName = findViewById(R.id.routineName);
         mDescription = findViewById(R.id.routineDescription);
 
-        String nameText = routine.name;
-        String DescriptionText = routine.description;
+        String nameText = MyApplication.routine.name;
+        String DescriptionText = MyApplication.routine.description;
 
         mName.setText(nameText);
         mDescription.setText(DescriptionText);
@@ -41,7 +35,8 @@ public class RoutineView extends AppCompatActivity {
         mStartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                myUser.setRoutine(routine.id, routine.workouts);
+                MyApplication.user.setRoutine(MyApplication.routine.id, MyApplication.routine.workouts);
+                //This will change to becoming Routine.class instead of MainActivity.class
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
                 finish();
             }
@@ -50,8 +45,7 @@ public class RoutineView extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        //This Should go to myRoutines page instead of MainActivity
-        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
     }
 }
